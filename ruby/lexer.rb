03515a0a907ed759a8970f910fe887a9aad3d465
Gabe Berke-Williams <gabebw@gabebw.com>
2015-02-22 15:17:36 -0500

    Add a lexer

---
+++ ruby/lexer.rb
@@ -0,0 +1,33 @@
+class Lexer
+  OPERATORS = %w(+ * - /)
+  OPERATOR_REGEX = Regexp.union(*OPERATORS)
+
+  def initialize(text)
+    @text = text
+    @tokens = []
+  end
+
+  attr_reader :tokens
+
+  def tokenize
+    i = 0
+    while i < @text.size
+      chunk = @text[i..-1]
+      if number = chunk[/\A(\d+)/, 1]
+        @tokens << [:NUMBER, number]
+        i += number.size
+      elsif space = chunk[/\A(\s+)/, 1]
+        # Ignore spaces
+        i += space.size
+      elsif operator = chunk[/\A(#{OPERATOR_REGEX})/, 1]
+        @tokens << [:OPERATOR, operator]
+        i += operator.size
+      end
+    end
+  end
+end
+
+lexer = Lexer.new("1 + 200 + 8 - 3 * 2 / 4")
+lexer.tokenize
+
+p lexer.tokens
