6e1b00d09908e8d0153218df169c3d699e16942a
Gabe Berke-Williams <gabebw@gabebw.com>
2015-02-27 15:58:47 -0500

    Add an ApplicativeParser
    `Parser` is an instance of Applicative, so we can use `<$>` (fmap), `<*>` (like
    `(Just (5 +)) <*> (Just 6) == Just 11`), and `*>`, which is `<*>` but discarding
    the left argument. It Just Works for each parser combination.

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,11 +1,15 @@
 import qualified MonadicParser as MP (evaluateExpression)
+import qualified ApplicativeParser as AP (evaluateExpression)
 import Rpn (evalToRpn)
 import Interpreter (interpret)
 
 main :: IO ()
 main = do
-    print rpnNodes
-    print $ "Should be " ++ show expectedResult  ++ ": " ++ (show $ interpret rpnNodes)
+    print monadicRpnNodes
+    print $ "[MonadicParser]     Should be " ++ show expectedResult  ++ ": " ++ (show $ interpret monadicRpnNodes)
+    print applicativeRpnNodes
+    print $ "[ApplicativeParser] Should be " ++ show expectedResult  ++ ": " ++ (show $ interpret applicativeRpnNodes)
     where
         expectedResult = 1 * 2 + 3 * 4 :: Float
-        rpnNodes = maybe [] evalToRpn $ MP.evaluateExpression "1 * 2+3 * 4"
+        monadicRpnNodes = maybe [] evalToRpn $ MP.evaluateExpression "1 * 2+3 * 4"
+        applicativeRpnNodes = maybe [] evalToRpn $ AP.evaluateExpression "1 * 2+3 * 4"
---
+++ haskell/src/ApplicativeParser.hs
@@ -0,0 +1,39 @@
+module ApplicativeParser (evaluateExpression) where
+
+import Text.ParserCombinators.Parsec
+import Nodes
+-- *> "points at the value you want to keep" (throws away the left)
+import Control.Applicative ((<$>), (<*>), (*>), pure)
+
+evaluateExpression  :: String -> Maybe ([Node])
+evaluateExpression input =
+    case parse parser "(unknown)" input of
+    Left _ -> Nothing
+    Right result -> Just result
+
+parser :: Parser [Node]
+parser = expressionParser
+
+expressionParser :: Parser [Node]
+expressionParser = (:) <$> numberNodeParser <*> manyFurtherExpressionsParser
+
+manyFurtherExpressionsParser :: Parser [Node]
+manyFurtherExpressionsParser = fmap concat $ many1 furtherExpressionParser
+
+-- sequence turns `[Parser Node]` into `Parser [Node]`
+furtherExpressionParser :: Parser [Node]
+furtherExpressionParser = sequence [operatorNodeParser, numberNodeParser]
+
+numberNodeParser :: Parser Node
+numberNodeParser = spaces *> (NumberNode <$> integerParser)
+
+integerParser :: Parser Float
+integerParser = fmap read $ many1 digit
+
+operatorNodeParser :: Parser Node
+operatorNodeParser = spaces *> operatorNode
+
+operatorNode :: Parser Node
+operatorNode = try (char '+' *> pure (OperatorNode Plus))
+        <|> try (char '-' *> pure (OperatorNode Minus))
+        <|> (char '*' *> pure (OperatorNode Times))
