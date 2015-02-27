bf8e63fcf28f676b7e6519e20229941b823d5c63
Gabe Berke-Williams <gabebw@gabebw.com>
2015-02-27 10:42:40 -0500

    Move types into their own module

---
+++ haskell/src/Nodes.hs
@@ -0,0 +1,18 @@
+module Nodes where
+
+import Data.Function (on)
+
+data Operator = Plus | Minus | Times deriving (Eq, Show)
+
+precedence :: Operator -> Int
+precedence Plus = 1
+precedence Minus = 1
+precedence Times = 2
+
+instance Ord Operator where
+    compare = compare `on` precedence
+
+data Node a = OperatorNode Operator | NumberNode Float
+instance Show (Node a) where
+    show (NumberNode f) = "NumberNode " ++ (show f)
+    show (OperatorNode o) = show o
