19fb255db6348591923abbb483a58f2046d8ca05
Gabe Berke-Williams <gabebw@gabebw.com>
2015-02-27 10:40:27 -0500

    Make operator a type

--- haskell/src/Calculator.hs
+++ haskell/src/Calculator.hs
@@ -1,14 +1,25 @@
 module Calculator where
 
 import Text.ParserCombinators.Parsec
+import Data.Function (on)
 
 -- number
 -- (operator number)+
 
-data Node a = Operator Char | Number Float
+data Operator = Plus | Minus | Times deriving (Eq, Show)
+
+precedence :: Operator -> Int
+precedence Plus = 1
+precedence Minus = 1
+precedence Times = 2
+
+instance Ord Operator where
+    compare = compare `on` precedence
+
+data Node a = OperatorNode Operator | NumberNode Float
 instance Show (Node a) where
-    show (Operator c) = "Operator " ++ [c]
-    show (Number f) = "Number " ++ (show f)
+    show (NumberNode f) = "NumberNode " ++ (show f)
+    show (OperatorNode o) = show o
 
 evaluateExpression  :: String -> Maybe ([Node a])
 evaluateExpression input =
@@ -24,15 +35,15 @@ expressionParser = do
     number <- numberParser
     spaces
     nodes <- many furtherExpressionParser
-    return $ [Number (read number)] ++ (concat nodes)
+    return $ [NumberNode (read number)] ++ (concat nodes)
 
 furtherExpressionParser :: Parser [Node a]
 furtherExpressionParser = do
     spaces
-    operator <- operatorParser
+    opChar <- operatorParser
     spaces
     number <- numberParser
-    return $ [Operator operator, Number (read number)]
+    return $ [OperatorNode (operatorFromChar opChar), NumberNode (read number)]
 
 integerParser :: Parser String
 integerParser = many digit
@@ -42,3 +53,9 @@ numberParser = integerParser
 
 operatorParser :: Parser Char
 operatorParser = char '+'
+
+operatorFromChar :: Char -> Operator
+operatorFromChar '+' = Plus
+operatorFromChar '-' = Minus
+operatorFromChar '*' = Times
+operatorFromChar _ = error "OH NO"
