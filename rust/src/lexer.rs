9933d7343aa3e8f34afb72a351305312a11a926a
Gabe Berke-Williams <gabebw@gabebw.com>
2015-03-09 21:53:31 -0400

    Rust: Lexer works

--- rust/src/lexer.rs
+++ rust/src/lexer.rs
@@ -1,3 +1,30 @@
-pub fn hello() -> String {
-    "Hello".to_string()
+fn is_operator(character: &str) -> bool {
+    let operators = ["+", "-", "*", "/", "^"];
+    for &operator in operators.iter() {
+        if character == operator {
+            return true;
+        }
+    }
+    return false;
+}
+
+pub fn lex(code: &str) -> Vec<String> {
+    let mut tokens : Vec<String> = vec![];
+    let mut index = 0;
+
+    for character in code.graphemes(true) {
+        if character == " " {
+            index += 1;
+        } else if is_operator(character) {
+            tokens.push(String::from_str(character));
+        } else {
+            if tokens.len() > index {
+                tokens[index].push_str(character);
+            }  else {
+                tokens.push(String::from_str(character));
+            }
+        }
+    }
+
+    return tokens
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,13 @@
+#![feature(collections)]
+
 // `mod lexer` tells Rust to look for src/lexer.rs or src/lexer/mod.rs
 mod lexer;
 
 fn main() {
-    println!("{}", lexer::hello())
+    let code = "100 + 2 / 3";
+    let lexed = lexer::lex(code);
+
+    for item in lexed.iter() {
+        println!("item: {}", item);
+    }
 }
